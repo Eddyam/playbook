1bfd17cbd50b6744ffcfdaf7edf78f462a8c949b
EddYam <edder_yamir@hotmail.com>
2022-04-25 22:40:11 -0500

    Agregando ejemplo0_3 Crear objeto con diferentes propiedades

---
+++ weekly_mission_2/examples0/example0-3.js
@@ -0,0 +1,19 @@
+// Objeto con diferentes propiedades 
+const myObject3 = {
+    name: "Skip",
+    age: 10,
+    nicknames: [
+        "Peluche",
+        "Negro"
+    ],
+    addres: {
+        zip_code: "50400",
+        street: "Mexico",
+        city: "Edo. Mexico"
+    }
+}
+
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3)
+console.log(myObject3.name)
+console.log(myObject3["addres"])
