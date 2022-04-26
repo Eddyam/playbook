f960d9afedcac33f8243530c44b3bfb5ee3879be
EddYam <edder_yamir@hotmail.com>
2022-04-25 22:34:42 -0500

    Agregando ejemplo0_2 Crear objeto con propiedades

---
+++ weekly_mission_2/examples0/example0-2.js
@@ -0,0 +1,9 @@
+// Ejemplo 2: Crear un objeto con propiedades
+
+const myObject2 = {
+    name: "Edder",
+    age: 28
+}
+
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObject2)
